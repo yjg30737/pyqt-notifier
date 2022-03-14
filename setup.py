cf9c4f33b24b5bc34d82e7aa704a58e6ecc68452
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-14 18:50:30 +0900

    Remove unused files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'pyqt_notifier.style': ['button.css'], 'pyqt_notifier.ico': ['close.svg']},
+    package_data={'pyqt_notifier.ico': ['close.svg']},
     description='PyQt Windows notifier show at bottom right of the desktop screen',
     url='https://github.com/yjg30737/pyqt-notifier.git',
     install_requires=[
