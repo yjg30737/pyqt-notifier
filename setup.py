f767a27330d00d282b11bc0f374aba171d76d45b
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-09 18:40:18 +0900

    Update setup.py, downgrade

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name='pyqt-notifier',
-    version='0.1.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     package_data={'pyqt_notifier.ico': ['close.svg']},
-    description='PyQt Windows notifier show at bottom right of the desktop screen',
+    description='PyQt notifier which shows at bottom right of the desktop screen',
     url='https://github.com/yjg30737/pyqt-notifier.git',
     long_description_content_type='text/markdown',
     long_description=long_description,
